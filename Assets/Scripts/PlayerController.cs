75d6f1a54668e7b197469b5b8eabef1e58d6b5d2
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-09-22 15:32:11 -0300

    Made jump and fixed gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,42 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float gravity;
+    public float jumpHeight;
 
     float X;
     float Z;
 
     Vector3 Move;
-    Vector3 Vel;
+    public Vector3 Vel;
 
     public CharacterController Cont;
+    public Transform groundCheck;
+    public float groundDist;
+    LayerMask groundMask;
+
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+        if(isGrounded && Vel.y < 0)
+        {
+            Vel.y = -2f;
+        }
+
         X = Input.GetAxis("Horizontal");
         Z = Input.GetAxis("Vertical");
 
         Move = transform.right * X + transform.forward * Z;
         Cont.Move(Move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Vel.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
 
         Vel.y += gravity * Time.deltaTime;
         Cont.Move(Vel * Time.deltaTime);
