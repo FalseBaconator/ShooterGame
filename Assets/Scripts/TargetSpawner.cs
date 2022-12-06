92fc208b7db4a607a74187a9ad1bdb34af1e92a6
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-12-06 10:55:40 -0400

    Made loop respawn more frequent

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -9,16 +9,19 @@ public class TargetSpawner : MonoBehaviour
     public GameObject targetStationary;
 
     public string targetTag;
+    public GameObject[] targets;
 
 
     public void OnTriggerEnter(Collider other)
     {
-        if(GameObject.FindGameObjectWithTag(targetTag) == null)
+        targets = GameObject.FindGameObjectsWithTag(targetTag);
+        foreach (GameObject target in targets)
         {
-            foreach (GameObject spawner in spawnerLocations)
-            {
-                GameObject.Instantiate(targetStationary, spawner.transform);
-            }
+            GameObject.Destroy(target);
+        }
+        foreach(GameObject spawner in spawnerLocations)
+        {
+            GameObject.Instantiate(targetStationary, spawner.transform);
         }
     }
 
